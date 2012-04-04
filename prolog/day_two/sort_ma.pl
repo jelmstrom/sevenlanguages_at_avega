69e6e8d4a8969d70db86fd96719333d558d34cd9
Majid Aram <majramo@gmail.com>
2012-04-04 19:07:42 +0200

    :w§:wq§

--- prolog/day_two/sort_ma.pl
+++ prolog/day_two/sort_ma.pl
@@ -1,7 +1,15 @@
 sortera([],[]).
 sortera([A],[A]).
 sortera([A,B|TAIL],AB):-
+newList(A,B,C),sortera(TAIL,C)
+
+newList(A,B,C):-
 (  A =< B
-    -> sortera([B|TAIL],C), AB = [A,C]
-    ;  sortera([A|TAIL],C), AB = [B,C] 
+    -> C = [C, A,B]
+    ; C = [C, B,A] 
+     ).
+newList(A,B,[]):-
+(  A =< B
+    -> C = [A,B]
+    ;  C = [B,A] 
      ).
