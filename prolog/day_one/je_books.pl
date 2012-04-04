9adbcd610afbca60cf70e0deb491f1c70edf32f5
= <johan.elmstrom@gmail.com>
2012-04-04 17:17:28 +0200

    fibonacci

--- prolog/day_one/je_books.pl
+++ prolog/day_one/je_books.pl
@@ -1,5 +1,8 @@
 book(lotr,tolkien).
 book(hitchikers_guide, adams).
-book(patterns,[gamma, vlissides, helm, johnson]).
+book(patterns, johnson).
+book(patterns, helm).
+book(patterns, vlissides).
+book(patterns, gamma).
 
 author(Author, Book) :- book(Book,Author).
