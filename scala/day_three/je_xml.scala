5147f711bd5f7e6409688d0b3417d90b9731f3de
= <johan.elmstrom@gmail.com>
2012-04-25 17:22:12 +0200

    xml gymnastics

---
+++ scala/day_three/je_xml.scala
@@ -0,0 +1,35 @@
+
+
+val movies =
+<movies>
+  <movie genre="action">Pirates of the Caribbean</movie>
+  <movie genre="action">Pirates of the Caribbean 2</movie>
+  <movie genre="fairytale">Edward Scissorhands</movie>
+</movies>
+
+
+println((movies \ "movie")(0) \ "@genre")
+
+val nodes = movies \ "movie"
+
+nodes foreach(node => println((node \ "@genre").text.equals("action")))
+
+println("Use filter method on nodes to look for genre")
+println(nodes.filter(node => node \ "@genre" exists(_.text == "action")))
+
+println("")
+println("Same but different")
+println((movies \\ "movie" filter { _ \ "@genre" exists (_.text == "action") }))
+
+println("")
+
+
+
+println("Filter with matcher")
+movies \ "_"  foreach {movie =>
+              movie match {
+              case value @ <movie>{_*}</movie> if (value \ "@genre").text.equals("action")=> println(value + " " + (value \ "@genre").text)
+              case _ =>
+             }
+          }
+
