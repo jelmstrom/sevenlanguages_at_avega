07766f8e0219aa9a36a816acd1fe54f8611b763a
= <johan.elmstrom@gmail.com>
2012-04-25 20:57:05 +0200

    concurrency

--- prolog/day_two/je_list_sort.pl
+++ prolog/day_two/je_list_sort.pl
@@ -1,11 +1,11 @@
 listsort(Org,Sorted) :- msort(Org,Sorted). %-- cheating
 
 
-insert_sort(List,Sorted):-sort(List,[],Sorted). %-- Accumulator
-insert_sort([],Acc,Acc).  %-- end of list, should be sorted
-insert_sort([Head|Tail],Acc,Sorted):-insert(Head,Tail,NxT,Max),write('insert ...'), insert_sort(NxT,[Max|Acc],Sorted). %-- move Head to correct position, rinse repeat..
+insert_sort(List,Sorted):-insert_sort(List,[],Sorted). %-- Accumulator
+insert_sort([],Acc,Acc).  %-- Fact! at end
+insert_sort([Head|Tail],Acc,Sorted):-insert(Head,Tail,NxT,Max), insert_sort(NxT,[Max|Acc],Sorted). %-- move Head to correct position, rinse repeat..
    
-insert(Head,[],[],Head). %-- single value - sorted...
+insert(Head,[],[],Head). %-- fact! no more items in tail. Max is Head.
 insert(Head,[Next|Tail],[Next|NxT],Max):-Head>Next,  insert(Head,Tail,NxT,Max). %-- Head greater than next - move it further back
 insert(Head,[Next|Tail],[Head|NxT],Max):-Head=<Next, insert(Next,Tail,NxT,Max). %-- Head smaller than next - move on..
 
