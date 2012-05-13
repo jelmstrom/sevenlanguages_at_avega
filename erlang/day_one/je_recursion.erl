542942a32fc13112beb2b7753eba66e48c6f313d
= <johan.elmstrom@gmail.com>
2012-05-13 22:45:06 +0200

    wordcount

--- erlang/day_one/je_recursion.erl
+++ erlang/day_one/je_recursion.erl
@@ -3,18 +3,16 @@
 -export([count_to_ten/1]).
 -export([count_to_ten/0]).
 -export([wordcount/1]).
--export([find_next/1]).
 
 
 %Strings are arrays of ints - useful!!!
-wordcount([]) ->0; % spaces after the last word ends up here - dont count
+wordcount([]) ->0;  %empty string to start with
+wordcount([32|[]]) -> 0; % trailing spaces are not a word
+wordcount([_|[]]) ->  1; % ends without trailing spaces 
 wordcount([32|Tail]) -> wordcount(Tail); % keep recursing until the first character isnt space
-wordcount(Rest) -> find_next(Rest). % Doesnt start with 32 - its a word.
+wordcount([_|[32|Tail]]) -> 1+wordcount(Tail); % The tail is teh beginning of a word
+wordcount([_|Tail]) -> wordcount(Tail). % not empty,end or starts with space - keep recursing
 
-
-find_next([]) ->1; % No space after last word - count it.
-find_next([32|Tail]) -> 1+wordcount(Tail); % Leading space - count as a word and continue
-find_next([_|Tail]) ->find_next(Tail). %recurse until next space found 
 
 %Prolog style matching any..
 count_to_ten() -> count_to_ten(ascending).
