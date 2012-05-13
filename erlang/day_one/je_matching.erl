19fc5b19560d413a2846e921db6b7a029d3ac28c
= <johan.elmstrom@gmail.com>
2012-05-13 21:17:10 +0200

    Matching two ways

--- erlang/day_one/je_matching.erl
+++ erlang/day_one/je_matching.erl
@@ -2,9 +2,11 @@
 -export([match/1]).
 -export([match2/1]).
 
+% By signature
 match({error,Message}) -> erlang:display("error:" ++Message);
 match(success) -> erlang:display("success").
 
+% By cases (why wourd I want to to this?)
 match2(Input) -> 
 	case Input of
 		success -> erlang:display("success");
