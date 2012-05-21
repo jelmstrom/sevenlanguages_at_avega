1f28f2d5edb28977ea7d551437d24c52651bab8b
= <johan.elmstrom@gmail.com>
2012-05-21 18:44:36 +0200

    list comprehension 2

--- erlang/day_two/je_tuples.erl
+++ erlang/day_two/je_tuples.erl
@@ -8,4 +8,8 @@ values(Tuples, Keyword) ->lists:map(fun(Item) -> element(2,Item) end,lists:filte
 filteredTuples(Tuples, Keyword) -> lists:filter(fun(Tuple) -> element(1,Tuple) == Keyword end, Tuples).
 tupleValues(Tuples, Keyword) -> [element(2,Item) || Item <- filteredTuples(Tuples,Keyword)].
 
+#SomeTuples = [{oo, "Ruby"}, {oo,"scala"}, {functional,"erlang"}, {functional,"scala"}, {oo,"java"}].
+#tupleValues(SomeTuples,oo).
+#values(SomeTuples, functional).
+#tupleValues(SomeTuples,functional).
 
