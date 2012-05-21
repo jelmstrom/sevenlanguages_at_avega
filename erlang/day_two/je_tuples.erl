32230660b1530334da603c76007863536409ee65
= <johan.elmstrom@gmail.com>
2012-05-21 18:37:43 +0200

    list comprehension

--- erlang/day_two/je_tuples.erl
+++ erlang/day_two/je_tuples.erl
@@ -1,9 +1,11 @@
 -module(je_tuples).
 -export([values/2]).
--export([shortvals/2]).
+-export([tupleValues/2]).
 
 
 
 values(Tuples, Keyword) ->lists:map(fun(Item) -> element(2,Item) end,lists:filter(fun(Tuple) -> element(1,Tuple) == Keyword  end, Tuples)).
 filteredTuples(Tuples, Keyword) -> lists:filter(fun(Tuple) -> element(1,Tuple) == Keyword end, Tuples).
-shortvals(Tuples, Keyword) -> [element(2,Item) || Item <- filteredTuples(Tuples,Keyword)].
+tupleValues(Tuples, Keyword) -> [element(2,Item) || Item <- filteredTuples(Tuples,Keyword)].
+
+
