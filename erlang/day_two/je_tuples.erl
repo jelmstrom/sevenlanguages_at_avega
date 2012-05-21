918bf34e17dc4b7a4325f8ec643c78601e2cecc1
= <johan.elmstrom@gmail.com>
2012-05-21 18:29:22 +0200

    list comprehension

--- erlang/day_two/je_tuples.erl
+++ erlang/day_two/je_tuples.erl
@@ -1,3 +1,9 @@
 -module(je_tuples).
 -export([values/2]).
+-export([shortvals/2]).
+
+
+
 values(Tuples, Keyword) ->lists:map(fun(Item) -> element(2,Item) end,lists:filter(fun(Tuple) -> element(1,Tuple) == Keyword  end, Tuples)).
+filteredTuples(Tuples, Keyword) -> lists:filter(fun(Tuple) -> element(1,Tuple) == Keyword end, Tuples).
+shortvals(Tuples, Keyword) -> [element(2,Item) || Item <- filteredTuples(Tuples,Keyword)].
