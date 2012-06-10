7eddf3fadda6c1f029b329748650eab445fcf1e3
= <johan.elmstrom@gmail.com>
2012-06-10 20:12:16 +0200

    renamed tests

--- clojure/day_one/je_big.clj
+++ clojure/day_one/je_big.clj
@@ -1,12 +1,12 @@
 (defn big [value len] (> (count value) len) )
 
-(defn test [value len expected] (if (= (big value len) expected)
+(defn testbig [value len expected] (if (= (big value len) expected)
                                           (println "Test passed")
                                             (println (str "Test for " value " longer than " len "faled to yield " expected))
                                 )
   )
 
-(test "hello" 3 true)
-(test "hello" 5 false)
-(test "hello" 6 false)
+(testbig "hello" 3 true)
+(testbig "hello" 5 false)
+(testbig "hello" 6 false)
 
